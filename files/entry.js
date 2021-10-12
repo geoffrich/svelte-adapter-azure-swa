5f4492b9f73b2871c9a62c5f19f11a45b8bffece
Geoff Rich <geoff.rich@alaskaair.com>
2021-10-12 10:56:03 -0700

    feat: initial commit

---
+++ files/entry.js
@@ -0,0 +1,44 @@
+// TODO hardcoding the relative location makes this brittle
+// Update once https://github.com/sveltejs/kit/pull/2579 merged
+import { init, render } from '../output/server/app.js';
+
+init();
+
+/**
+ * @typedef {import('@azure/functions').AzureFunction} AzureFunction
+ * @typedef {import('@azure/functions').Context} Context
+ * @typedef {import('@azure/functions').HttpRequest} HttpRequest
+ */
+
+/**
+ * @param {Context} context
+ */
+export async function index(context) {
+	const { method, headers, rawBody: body } = context.req;
+	// because we proxy all requests to the render function, the original URL in the request is /api/__render
+	// this header contains the URL the user requested
+	// TODO: replace with documented solution from https://github.com/Azure/static-web-apps/issues/580
+	// not sure if this solution is stable
+	const originalUrl = headers['x-ms-original-url'];
+	const { pathname, searchParams } = new URL(originalUrl);
+
+	const encoding = headers['content-encoding'] || 'utf-8';
+	const rawBody = typeof body === 'string' ? Buffer.from(body, encoding) : body;
+
+	const rendered = await render({
+		method,
+		headers,
+		path: pathname,
+		query: searchParams,
+		rawBody
+	});
+
+	const { status, headers: resHeaders, body: resBody } = rendered;
+
+	context.res = {
+		status,
+		body: resBody,
+		headers: resHeaders,
+		rawBody
+	};
+}
---
+++ index.js
@@ -0,0 +1,60 @@
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import esbuild from 'esbuild';
+
+/**
+ * @typedef {import('esbuild').BuildOptions} BuildOptions
+ */
+
+/** @type {import('.')} */
+export default function (options) {
+	return {
+		name: 'adapter-azure-swa',
+
+		async adapt({ utils }) {
+			const publish = 'build';
+			const staticDir = join(publish, 'static');
+			const apiDir = join('api', 'render');
+			const entry = '.svelte-kit/azure-swa/entry.js';
+
+			utils.log.minor(`Publishing to "${publish}"`);
+
+			utils.rimraf(publish);
+			utils.rimraf(apiDir);
+
+			const files = fileURLToPath(new URL('./files', import.meta.url));
+
+			utils.log.minor('Generating serverless function...');
+			utils.copy(join(files, 'entry.js'), entry);
+			utils.copy(
+				join(files, 'staticwebapp.config.json'),
+				join(publish, 'staticwebapp.config.json')
+			);
+			utils.copy(join(files, 'api'), apiDir);
+
+			/** @type {BuildOptions} */
+			const default_options = {
+				entryPoints: [entry],
+				outfile: join(apiDir, 'index.js'),
+				bundle: true,
+				inject: [join(files, 'shims.js')],
+				platform: 'node'
+			};
+
+			const build_options =
+				options && options.esbuild ? await options.esbuild(default_options) : default_options;
+
+			await esbuild.build(build_options);
+
+			utils.log.minor('Prerendering static pages...');
+			await utils.prerender({
+				dest: staticDir
+			});
+
+			utils.log.minor('Copying assets...');
+			utils.copy_static_files(staticDir);
+			utils.copy_client_files(staticDir);
+		}
+	};
+}
