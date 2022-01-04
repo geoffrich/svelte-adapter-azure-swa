2a8a5f9726dc7204050788b6c6d806636c762d18
Geoff Rich <geoff.rich@alaskaair.com>
2022-01-04 15:40:02 -0800

    feat: support SK next-208

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { writeFileSync } from 'fs';
-import { join } from 'path';
+import { join, posix } from 'path';
 import { fileURLToPath } from 'url';
 import esbuild from 'esbuild';
 
@@ -8,54 +8,70 @@ import esbuild from 'esbuild';
  */
 
 /** @type {import('.')} */
-export default function (options) {
+export default function () {
 	return {
 		name: 'adapter-azure-swa',
 
-		async adapt({ utils }) {
+		// implementation based on the vercel adapter
+		async adapt(builder) {
+			const tmp = builder.getBuildDirectory('azure-tmp');
 			const publish = 'build';
 			const staticDir = join(publish, 'static');
 			const apiDir = join('api', 'render');
-			const entry = '.svelte-kit/azure-swa/entry.js';
+			const entry = `${tmp}/entry.js`;
+			builder.log.minor(`Publishing to "${publish}"`);
 
-			utils.log.minor(`Publishing to "${publish}"`);
+			builder.rimraf(tmp);
+			builder.rimraf(publish);
+			builder.rimraf(apiDir);
 
-			utils.rimraf(publish);
-			utils.rimraf(apiDir);
+			builder.log.minor('Prerendering static pages...');
+			await builder.prerender({
+				dest: staticDir
+			});
 
 			const files = fileURLToPath(new URL('./files', import.meta.url));
 
-			utils.log.minor('Generating serverless function...');
-			utils.copy(join(files, 'entry.js'), entry);
-			utils.copy(
+			builder.log.minor('Generating serverless function...');
+
+			// use posix because of https://github.com/sveltejs/kit/pull/3200
+			const relativePath = posix.relative(tmp, builder.getServerDirectory());
+
+			builder.copy(join(files, 'entry.js'), entry, {
+				replace: {
+					APP: `${relativePath}/app.js`,
+					MANIFEST: './manifest.js'
+				}
+			});
+
+			writeFileSync(
+				`${tmp}/manifest.js`,
+				`export const manifest = ${builder.generateManifest({
+					relativePath
+				})};\n`
+			);
+
+			builder.copy(
 				join(files, 'staticwebapp.config.json'),
 				join(publish, 'staticwebapp.config.json')
 			);
-			utils.copy(join(files, 'api'), apiDir);
+
+			builder.copy(join(files, 'api'), apiDir);
 
 			/** @type {BuildOptions} */
 			const default_options = {
 				entryPoints: [entry],
 				outfile: join(apiDir, 'index.js'),
 				bundle: true,
-				inject: [join(files, 'shims.js')],
 				platform: 'node',
 				target: 'node12'
 			};
 
-			const build_options =
-				options && options.esbuild ? await options.esbuild(default_options) : default_options;
+			await esbuild.build(default_options);
 
-			await esbuild.build(build_options);
-
-			utils.log.minor('Prerendering static pages...');
-			await utils.prerender({
-				dest: staticDir
-			});
-
-			utils.log.minor('Copying assets...');
-			utils.copy_static_files(staticDir);
-			utils.copy_client_files(staticDir);
+			builder.log.minor('Copying assets...');
+			builder.writeStatic(staticDir);
+			builder.writeClient(staticDir);
 		}
 	};
 }
