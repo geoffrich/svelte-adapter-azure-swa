61c374d7deb2ce0af2a503b6cdaaab9aa762b3c3
Logan Dam <biltongza@users.noreply.github.com>
2022-02-03 21:34:17 +0200

    feat: support SvelteKit 1.0.0-next.234 (#29)
    * update adapter to support sveltekit 1.0.0-next.234
    
    * Refactor request/response creation
    
    * Missing await
    
    Co-authored-by: Geoff Rich <4992896+geoffrich@users.noreply.github.com>

--- files/entry.js
+++ files/entry.js
@@ -20,38 +20,62 @@ const app = new App(manifest);
  * @param {Context} context
  */
 export async function index(context) {
-	const { method, headers, rawBody: body } = context.req;
-	// because we proxy all requests to the render function, the original URL in the request is /api/__render
-	// this header contains the URL the user requested
-	const originalUrl = headers['x-ms-original-url'];
-	const url = new URL(originalUrl);
-
-	const rawBody = typeof body === 'string' ? Buffer.from(body, 'utf-8') : body;
-	const request = {
-		url,
-		method,
-		headers,
-		rawBody
-	};
+	const request = toRequest(context);
 
 	if (debug) {
 		context.log(`Request: ${JSON.stringify(request)}`);
 	}
 
 	const rendered = await app.render(request);
-
-	const { status, headers: resHeaders, body: resBody } = rendered;
-
-	const response = {
-		status,
-		body: resBody,
-		headers: resHeaders,
-		rawBody
-	};
+	const response = await toResponse(rendered);
 
 	if (debug) {
 		context.log(`Response: ${JSON.stringify(response)}`);
 	}
 
 	context.res = response;
+}
+
+/**
+ * @param {Context} context
+ * @returns {Request}
+ * */
+function toRequest(context) {
+	const { method, headers, rawBody: body } = context.req;
+	// because we proxy all requests to the render function, the original URL in the request is /api/__render
+	// this header contains the URL the user requested
+	const originalUrl = headers['x-ms-original-url'];
+
+	/** @type {RequestInit} */
+	const init = {
+		method,
+		headers: new Headers(headers)
+	};
+
+	if (method !== 'GET' && method !== 'HEAD') {
+		init.body = typeof body === 'string' ? Buffer.from(body, 'utf-8') : body;
+	}
+
+	return new Request(originalUrl, init);
+}
+
+/**
+ * @param {Response} rendered
+ * @returns {Promise<Record<string, any>>}
+ */
+async function toResponse(rendered) {
+	const { status } = rendered;
+	const resBody = await rendered.text();
+
+	/** @type {Record<string, string>} */
+	const resHeaders = {};
+	rendered.headers.forEach((value, key) => {
+		resHeaders[key] = value;
+	});
+
+	return {
+		status,
+		body: resBody,
+		headers: resHeaders
+	};
 }
