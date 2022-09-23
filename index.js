940f2684f4204f392ae5dbe74aaaa10423c1c75f
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2022-09-23 16:41:24 -0400

    allow wildcard routes

--- index.js
+++ index.js
@@ -8,21 +8,12 @@ import esbuild from 'esbuild';
  */
 
 const ssrFunctionRoute = '/api/__render';
-
-/**
- * Validate the static web app configuration does not override the minimum config for the adapter to work correctly.
- * @param config {import('./types/swa').CustomStaticWebAppConfig}
- * */
-function validateCustomConfig(config) {
-	if (config) {
-		if ('navigationFallback' in config) {
-			throw new Error('customStaticWebAppConfig cannot override navigationFallback.');
-		}
-		if (config.routes && config.routes.find((route) => route.route === '*')) {
-			throw new Error(`customStaticWebAppConfig cannot override '*' route.`);
-		}
-	}
-}
+// These are the methods that don't have to be SSR if they were pre-rendered.
+const staticMethods = ['GET','HEAD','OPTIONS'];
+// These are the methods that must always be SSR.
+const ssrMethods = ['CONNECT','DELETE','PATCH','POST','PUT','TRACE'];
+// This is the phrase that will be replaced with ssrFunctionRoute in custom configurations that use it.
+const ssrTrigger = 'ssr';
 
 /** @type {import('.').default} */
 export default function ({
@@ -34,36 +25,31 @@ export default function ({
 		name: 'adapter-azure-swa',
 
 		async adapt(builder) {
-			validateCustomConfig(customStaticWebAppConfig);
+			/** @type {import('./types/swa').StaticWebAppConfig} */
+			const swaConfig = JSON.parse(JSON.stringify(customStaticWebAppConfig));
+			swaConfig.navigationFallback = {
+				rewrite: ssrFunctionRoute,
+				...swaConfig.navigationFallback
+			};
+			swaConfig.platform = {
+				apiRuntime: 'node:16',
+				...swaConfig.platform
+			};
+			swaConfig.routes = [];
 
-			if (!customStaticWebAppConfig.routes) {
-				customStaticWebAppConfig.routes = [];
+			if (swaConfig.navigationFallback.rewrite === ssrTrigger) {
+				swaConfig.navigationFallback.rewrite = ssrFunctionRoute;
 			}
 
-			/** @type {import('./types/swa').StaticWebAppConfig} */
-			const swaConfig = {
-				...customStaticWebAppConfig,
-				routes: [
-					...customStaticWebAppConfig.routes,
-					{
-						route: '*',
-						methods: ['POST', 'PUT', 'DELETE'],
-						rewrite: ssrFunctionRoute
-					},
-					{
-						route: `/${builder.config.kit.appDir}/immutable/*`,
-						headers: {
-							'cache-control': 'public, immutable, max-age=31536000'
-						}
-					}
-				],
-				navigationFallback: {
-					rewrite: ssrFunctionRoute
-				},
-				platform: {
-					apiRuntime: 'node:16'
-				}
-			};
+			if (swaConfig.navigationFallback.rewrite !== ssrFunctionRoute) {
+				builder.log.warn(
+					`Setting navigationFallback.rewrite to a value other than '${ssrTrigger}' will prevent SSR!`
+				);
+			}
+
+			if (!swaConfig.platform.apiRuntime.startsWith('node:')) {
+				throw new Error('The platform.apiRuntime must be set to a node version, such as node:16');
+			}
 
 			if (!existsSync(join('api', 'package.json'))) {
 				throw new Error(
@@ -112,7 +98,7 @@ export default function ({
 				outfile: join(apiDir, 'index.js'),
 				bundle: true,
 				platform: 'node',
-				target: 'node16',
+				target: `${swaConfig.platform.apiRuntime.replace(':', '')}`,
 				external: esbuildOptions.external
 			};
 
@@ -122,21 +108,136 @@ export default function ({
 			builder.writeClient(staticDir);
 			builder.writePrerendered(staticDir);
 
+			if (!customStaticWebAppConfig.routes) {
+				customStaticWebAppConfig.routes = [];
+			}
+
+			/** @type {Record<string,boolean>} */
+			let handledRoutes = {};
+			/** @type {import('./types/swa').Route} */
+			let wildcardRoute = {};
+			for (const route of customStaticWebAppConfig.routes) {
+				if (route.route === undefined || !route.route.length) {
+					throw new Error(
+						'A route pattern is required for each route. https://learn.microsoft.com/en-us/azure/static-web-apps/configuration#routes'
+					);
+				}
+				handledRoutes[route.route] = true;
+				const methods = route.methods || [];
+				if (route.rewrite === ssrTrigger) {
+					route.rewrite = ssrFunctionRoute;
+				}
+				if (['/*', '*'].includes(route.route)) {
+					wildcardRoute = route;
+				}
+				if (methods.length === 0) {
+					if (
+						['/index.html', '/'].includes(route.route) &&
+						!builder.prerendered.paths.includes('/')
+					) {
+						// The root route must be fully SSR because it was not rendered. No need to split the route.
+						swaConfig.routes.push({
+							rewrite: ssrFunctionRoute,
+							...route
+						});
+					} else {
+						// This route catches all methods, but we don't want to force SSR for all methods, so will split the rule.
+						swaConfig.routes.push({
+							...route,
+							methods: staticMethods
+						});
+						swaConfig.routes.push({
+							rewrite: ssrFunctionRoute,
+							...route,
+							methods: ssrMethods
+						});
+					}
+				} else if (route.methods.some((r) => ssrMethods.includes(r))) {
+					const routeSSRMethods = methods.filter((m) => ssrMethods.includes(m));
+					if (routeSSRMethods.length === methods.length) {
+						// This route is only for SSR methods, so we'll rewrite the single rule.
+						swaConfig.routes.push({
+							rewrite: ssrFunctionRoute,
+							...route
+						});
+					} else {
+						if (
+							['/index.html', '/'].includes(route.route) &&
+							!builder.prerendered.paths.includes('/')
+						) {
+							// This special route must be SSR because it was not pre-rendered.
+							swaConfig.routes.push({
+								rewrite: ssrFunctionRoute,
+								...route
+							});
+						} else {
+							// This route is for some methods that must be SSR, but not all. We'll split it.
+							swaConfig.routes.push({
+								rewrite: ssrFunctionRoute,
+								...route,
+								methods: routeSSRMethods
+							});
+							swaConfig.routes.push({
+								...route,
+								methods: methods.filter((m) => staticMethods.includes(m))
+							});
+						}
+					}
+				} else {
+					if (
+						['/index.html', '/'].includes(route.route) &&
+						!builder.prerendered.paths.includes('/')
+					) {
+						// This special route must be SSR because it was not pre-rendered.
+						swaConfig.routes.push({
+							rewrite: ssrFunctionRoute,
+							...route
+						});
+					} else {
+						// None of the methods in this route must be SSR, so accept it as-is.
+						swaConfig.routes.push({ ...route });
+					}
+				}
+			}
+
+			// Now that the specified rules have been processed, let's make sure the wildcard for SSR is there.
+			if (!handledRoutes['*'] && !handledRoutes['/*']) {
+				handledRoutes['*'];
+				swaConfig.routes.push({
+					route: '*',
+					methods: ssrMethods,
+					rewrite: ssrFunctionRoute
+				});
+			}
+
+			handledRoutes[`/${builder.config.kit.appDir}/immutable/*`] = true;
+			swaConfig.routes.push({
+				...wildcardRoute,
+				route: `/${builder.config.kit.appDir}/immutable/*`,
+				headers: {
+					'cache-control': 'public, immutable, max-age=31536000'
+				}
+			});
+
 			if (!builder.prerendered.paths.includes('/')) {
 				// Azure SWA requires an index.html to be present
 				// If the root was not pre-rendered, add a placeholder index.html
 				// Route all requests for the index to the SSR function
 				writeFileSync(`${staticDir}/index.html`, '');
-				swaConfig.routes.push(
-					{
-						route: '/index.html',
-						rewrite: ssrFunctionRoute
-					},
-					{
-						route: '/',
-						rewrite: ssrFunctionRoute
-					}
-				);
+				if (!handledRoutes['/index.html']) {
+					swaConfig.routes.push({
+						rewrite: ssrFunctionRoute,
+						...wildcardRoute,
+						route: '/index.html'
+					});
+				}
+				if (!handledRoutes['/']) {
+					swaConfig.routes.push({
+						rewrite: ssrFunctionRoute,
+						...wildcardRoute,
+						route: '/'
+					});
+				}
 			}
 
 			writeFileSync(`${publish}/staticwebapp.config.json`, JSON.stringify(swaConfig));
