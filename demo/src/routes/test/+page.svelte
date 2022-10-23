918de29eab11866fcc2adb047af7f2a2fdf02d8f
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-10-23 16:06:19 -0700

    Add repro

---
+++ demo/src/routes/test/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	async function getRandomNumber() {
+		const res = await fetch(`/random`);
+		const json = await res.json();
+
+		if (res.ok) {
+			return json;
+		} else {
+			throw new Error(JSON.stringify(json));
+		}
+	}
+
+	let promise = getRandomNumber();
+
+	function handleClick() {
+		promise = getRandomNumber();
+	}
+</script>
+
+<button on:click={handleClick}> generate random number </button>
+
+{#await promise}
+	<p>...waiting</p>
+{:then number}
+	<p>The number is {number}</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
