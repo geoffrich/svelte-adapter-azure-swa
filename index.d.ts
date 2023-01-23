5f2e741526bd2a4b125f487e75bb02aa6fc4198e
Tobias Laundal <tobias@laundal.no>
2023-01-23 21:06:19 +0000

    fixup! feat: expose client principal through platform

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,9 @@ import { Adapter } from '@sveltejs/kit';
 import { CustomStaticWebAppConfig } from './types/swa';
 import esbuild from 'esbuild';
 
-type Options = {
+export * from './types/swa';
+
+export type Options = {
 	debug?: boolean;
 	customStaticWebAppConfig?: CustomStaticWebAppConfig;
 	esbuildOptions?: Pick<esbuild.BuildOptions, 'external'>;
