0548971d7498675d27cf4b9f172aa0cab141842a
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2023-07-02 15:14:21 -0700

    Revert "try bumping demo to node:18"
    This reverts commit b0cb05517b7dfde805b4f2a2398d0abc8c05cf72.

--- demo/svelte.config.js
+++ demo/svelte.config.js
@@ -3,13 +3,7 @@ import adapter from 'svelte-adapter-azure-swa';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			customStaticWebAppConfig: {
-				platform: {
-					apiRuntime: 'node:18'
-				}
-			}
-		})
+		adapter: adapter()
 	}
 };
 
