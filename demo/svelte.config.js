4befd66015b5ce7d5388c42199cd7239d8cf6a57
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2023-11-19 16:15:53 -0800

    Scaffold new function for demo

--- demo/svelte.config.js
+++ demo/svelte.config.js
@@ -3,7 +3,9 @@ import adapter from 'svelte-adapter-azure-swa';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			apiDir: './func'
+		})
 	}
 };
 
