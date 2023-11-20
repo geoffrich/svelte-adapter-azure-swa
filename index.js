3fe92203dadf7e76d2c5f0cb71285c22fe1031fe
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2023-11-19 17:22:19 -0800

    feat: Node 18 as default

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ export function generateConfig(customStaticWebAppConfig, appDir) {
 			rewrite: ssrFunctionRoute
 		},
 		platform: {
-			apiRuntime: 'node:16',
+			apiRuntime: 'node:18',
 			...customStaticWebAppConfig.platform
 		}
 	};
