53ec98a4a6e13364ff9f9ff983d791ca1ae5297a
Christian Köberl <123199+derkoe@users.noreply.github.com>
2024-07-15 08:40:21 +0000

    convert external function to v4

--- demo/func/HelloWorld/index.js
+++ demo/func/HelloWorld/index.js
@@ -1,13 +1,22 @@
-module.exports = async function (context, req) {
-	context.log('JavaScript HTTP trigger function processed a request.');
+const { app } = require('@azure/functions');
 
-	const name = req.query.name || (req.body && req.body.name);
-	const responseMessage = name
-		? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
-		: 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
+app.http('httpTrigger1', {
+	methods: ['GET', 'POST'],
+	handler: async (req, context) => {
+		context.log('JavaScript HTTP trigger function processed a request.');
 
-	context.res = {
-		// status: 200, /* Defaults to 200 */
-		body: responseMessage
-	};
-};
+		let name;
+		if (req.query.has('name')) {
+			name = req.query.get('name')
+		} else {
+			let body = await req.json();
+			name = body.name;
+		}
+
+		const responseMessage = name
+			? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
+			: 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
+
+		return { body: responseMessage };
+	}
+});
