13a64bcb70cf26a8987d37f87c7d2dd9bff91b0d
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2025-03-03 19:45:09 -0800

    hello world returning a stream

--- demo/func/HelloWorld/index.js
+++ demo/func/HelloWorld/index.js
@@ -1,22 +1,27 @@
 const { app } = require('@azure/functions');
+import { Readable } from 'stream';
 
 app.http('HelloWorld', {
 	methods: ['GET', 'POST'],
 	handler: async (req, context) => {
 		context.log('JavaScript HTTP trigger function processed a request.');
-
-		let name;
-		if (req.query.has('name')) {
-			name = req.query.get('name');
-		} else if (req.headers.get('content-type') === 'application/json') {
-			let body = await req.json();
-			name = body.name;
-		}
-
-		const responseMessage = name
-			? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
-			: 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
-
-		return { body: responseMessage };
+		return { body: createNumberStream() };
 	}
 });
+
+function createNumberStream() {
+	let current = 1;
+
+	return new Readable({
+		objectMode: true, // Ensures numbers are treated as objects, not buffers
+		read() {
+			if (current <= 10) {
+				setTimeout(() => {
+					this.push(String(current++)); // Push the number as a string
+				}, 1000);
+			} else {
+				this.push(null); // End the stream
+			}
+		}
+	});
+}
