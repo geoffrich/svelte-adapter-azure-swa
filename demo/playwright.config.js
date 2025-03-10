ea33e8de6abb54b57cde558855e391fdf0cf1976
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-03-10 12:15:36 +0100

    prep for ms issue

--- demo/playwright.config.js
+++ demo/playwright.config.js
@@ -1,11 +1,14 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-	webServer: process.env.CI
-		? undefined
+	webServer: process.env.SWA
+		? {
+			command: 'npm run build && npm run swa',
+			port: 4280
+		}
 		: {
-				command: 'npm run build && npm run preview',
-				port: 4173
-			}
+			command: 'npm run build && npm run preview',
+			port: 4173
+		}
 };
 
 export default config;
--- demo/tests/app.spec.js
+++ demo/tests/app.spec.js
@@ -1,5 +1,10 @@
 import { expect, test } from '@playwright/test';
 
+test('home page has expected h1', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.locator('h1')).toBeVisible();
+});
+
 test('about page has expected h1', async ({ page }) => {
 	await page.goto('/about');
 	expect(await page.textContent('h1')).toBe('About this app');
