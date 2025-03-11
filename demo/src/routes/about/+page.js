56e0d4dff73db7d98caa05ef728af934aeb7a5ab
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-03-11 12:21:28 +0100

    update demo for SWA tests

--- demo/src/routes/about/+page.js
+++ demo/src/routes/about/+page.js
@@ -6,4 +6,4 @@ export const csr = dev;
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-// export const prerender = true;
+export const prerender = !process.env.SWA; // swa at the moment has issue with serving prerendered assets
