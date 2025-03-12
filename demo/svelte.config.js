3de75214c6860ed5bdda5b760402daf5a859c50a
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-03-12 12:15:13 +0100

    demo node apiRuntime

--- demo/svelte.config.js
+++ demo/svelte.config.js
@@ -1,10 +1,20 @@
 import adapter from 'svelte-adapter-azure-swa';
 
+const [major, minor, patch] = process.versions.node.split('.').map(Number);
+const apiRuntime = `node:${major}`;
+console.warn('#'.repeat(100));
+console.warn(`Using API runtime: ${apiRuntime}`);
+console.warn('#'.repeat(100));
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			apiDir: './func'
+			apiDir: './func',
+			customStaticWebAppConfig: {
+				platform: {
+					apiRuntime
+				}
+			}
 		})
 	}
 };
