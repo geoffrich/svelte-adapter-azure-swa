5bb79f620807ada55225de4ecc299fc21829c733
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-04-03 19:03:27 +0200

    tests

--- demo/tests/app.spec.js
+++ demo/tests/app.spec.js
@@ -1,17 +1,17 @@
 import { expect, test } from '@playwright/test';
 
 test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
+	await page.goto('/', { waitUntil: 'networkidle', timeout: 60000 });
 	await expect(page.locator('h1')).toBeVisible();
 });
 
 test('about page has expected h1', async ({ page }) => {
-	await page.goto('/about');
+	await page.goto('/about', { waitUntil: 'networkidle', timeout: 60000 });
 	expect(await page.textContent('h1')).toBe('About this app');
 });
 
 test('submits sverdle guess', async ({ page }) => {
-	await page.goto('/sverdle');
+	await page.goto('/sverdle', { waitUntil: 'networkidle', timeout: 60000 });
 	// wait for the sveltekit to run hydration
 	// Otherwise the test will fail
 	// await page.waitForTimeout(2000);
