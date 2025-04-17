efb1cffe5e07329a4a93cde28e8b53098f45125d
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-04-17 20:49:52 +0200

    generate swa config interface from JSON schema

--- types/swa.d.ts
+++ types/swa.d.ts
@@ -8,44 +8,6 @@ export interface StaticWebAppConfig extends AzureStaticWebAppsConfigurationFile 
 
 export type CustomStaticWebAppConfig = Omit<StaticWebAppConfig, 'navigationFallback'>;
 
-export interface Route {
-	route: string;
-	methods?: HttpMethod[];
-	rewrite?: string;
-	redirect?: string;
-	statusCode?: number;
-	headers?: Record<string, string>;
-	allowedRoles?: string[];
-}
-
-export interface NavigationFallback {
-	rewrite: string;
-	exclude?: string[];
-}
-
-export type HttpMethod =
-	| 'GET'
-	| 'HEAD'
-	| 'POST'
-	| 'PUT'
-	| 'DELETE'
-	| 'CONNECT'
-	| 'OPTIONS'
-	| 'TRACE'
-	| 'PATCH';
-
-export interface ResponseOverride {
-	rewrite?: string;
-	statusCode?: number;
-	redirect?: string;
-}
-
-export type OverridableResponseCodes = '400' | '401' | '403' | '404';
-
-export interface Platform {
-	apiRuntime: string;
-}
-
 /**
  * Client principal as presented to the render functions of a SWA.
  *
