b1f73817fd60fe4aa9e6a63277b3d7c98b1236e4
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-04-18 14:48:10 +0200

    update coverage, node engine in /demo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		exclude: [...configDefaults.exclude, 'demo/**']
-	}
+		exclude: [...configDefaults.exclude, 'demo/**'],
+		coverage: {
+			provider: 'istanbul',
+			exclude: [...configDefaults.exclude, 'demo/**'],
+			reporter: ['text', 'html', 'clover', 'json', 'lcov']
+		}
+	},
 });
