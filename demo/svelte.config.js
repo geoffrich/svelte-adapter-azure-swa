d46d363d1ada08fab40e4102e0d93d4079d45bbb
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-04-27 21:55:54 +0200

    update

--- demo/svelte.config.js
+++ demo/svelte.config.js
@@ -2,8 +2,15 @@ import adapter from 'svelte-adapter-azure-swa';
 const [major] = process.versions.node.split('.').map(Number);
 let NODE_API_RUNTIME = process.env.NODE_API_RUNTIME;
 console.warn('#'.repeat(100));
-if (!NODE_API_RUNTIME || NODE_API_RUNTIME.length === 0 || NODE_API_RUNTIME.split(':')[1] !== '') {
-	console.warn(`NODE_API_RUNTIME is not set. Defaulting to Node.js ${major} runtime.`);
+if (
+	!NODE_API_RUNTIME ||
+	NODE_API_RUNTIME.length === 0 ||
+	!NODE_API_RUNTIME.startsWith('node:') ||
+	NODE_API_RUNTIME.split(':')[1] !== ''
+) {
+	console.warn(
+		`NODE_API_RUNTIME is not set or not set properly (${NODE_API_RUNTIME}). Defaulting to Node.js ${major} runtime.`
+	);
 	NODE_API_RUNTIME = major;
 }
 console.warn(`Using API runtime: ${NODE_API_RUNTIME}`);
