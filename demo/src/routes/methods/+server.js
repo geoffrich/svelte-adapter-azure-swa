288221af0c3d52714936686f79f6423529b85731
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2025-11-26 12:37:09 -0800

    fix: support PATCH verb

---
+++ demo/src/routes/methods/+server.js
@@ -0,0 +1,19 @@
+export const GET = () => {
+	return new Response('getted');
+};
+
+export const POST = () => {
+	return new Response('posted');
+};
+
+export const PUT = () => {
+	return new Response('putted');
+};
+
+export const DELETE = () => {
+	return new Response('deleted');
+};
+
+export const PATCH = () => {
+	return new Response('patched');
+};
